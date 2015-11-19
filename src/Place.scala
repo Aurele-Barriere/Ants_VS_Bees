f15a03034272d93c78928b44bc00332930fd31f6
Aurèle <aurele.barriere@gmail.com>
2015-11-19 10:33:11 +0100

    a place can be a tunnel, a hive or a cell(in a hive)

--- src/Place.scala
+++ src/Place.scala
@@ -20,6 +20,9 @@ class Tunnel (p :Point, n :String, ex : Place, ico :ImageIcon) extends Place (p,
   def addbee (b :List[Insect]) = {
     bees = bees ::: b
   }
+  def removebee (b : Insect) = {
+  bees =  bees diff List(b)
+  }
   def removeant = {
     ant = false
   }
@@ -29,5 +32,25 @@ class Tunnel (p :Point, n :String, ex : Place, ico :ImageIcon) extends Place (p,
        typeant = t
     }
   }
-  
+  class Cell (p : Point, n : String, t :String) extends Place (p, n) {
+    val typeant :String = t
+    var is_selected :Boolean = false
+    
+    def buy_ant(money : Int, cost : Int, tun : Tunnel) = {
+      if (money > cost && !tun.ant) {
+        tun.ant = true
+        tun.typeant = typeant
+        //money = money - cost. how to remove money? pointers?
+      }
+    }
+  }
+  class Hive (L : List[Cell]) extends Place (new Point(0,0), "Hive") {
+    val Cells : List[Cell] = L
+    def select (c: Cell) {
+      for (cell <- Cells) {
+        cell.is_selected = false
+      }
+      c.is_selected = true
+    }
+  }
 }
