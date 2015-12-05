e5f0c41ec6650f1a1aaf3dfc84a169d38489d837
Remy Sun <Remy.Sun@ens-rennes.fr>
2015-12-06 00:12:07 +0100

    Save.scala is now readable, hopefully I did not break anything

--- src/Save.scala
+++ src/Save.scala
@@ -3,26 +3,47 @@ import java.awt.Point
 
 object savestate {
   val fileName = "saves.txt"
+  
+  // Saving game data to file "saves.txt"
+  
   def printToFile(content: String, location: String = fileName) =
     Some(new java.io.PrintWriter(location)).foreach { f => try { f.write(content) } finally { f.close } }
-  def save() = {
-    var saving = AntsBees.state.purse.money.toString() + "\n" // Money
-    saving += AntsBees.state.uniqueUnits.toString() + "\n" // Number of unique units
-    saving += AntsBees.state.lost.toString() + "\n" // Is the game lost?
+  
+  def save() = { 
+    
+    // Keeping game data in saving
+    
+      // General game state
+    
+    var saving = AntsBees.state.purse.money.toString() + "\n" 
+    saving += AntsBees.state.uniqueUnits.toString() + "\n" 
+    saving += AntsBees.state.lost.toString() + "\n" // 
     saving += AntsBees.state.timer.toString() + "\n"
+    
+      // Information relative to caves
+    
     var caves = AntsBees.state.Caves
-    while (caves != Nil) {
+    while (caves != Nil) { // Looking at every cave for bottom to top
       var cave = caves.head
       caves = caves.tail
+      
+        // Information relative to tunnels
+      
       var tunnels = cave.Tunnels
-      while (tunnels != Nil) {
+      while (tunnels != Nil) { // Looking at every tunnel of a cave from right to left
         val tunnel = tunnels.head
         tunnels = tunnels.tail
+        
+          // What type of tunnel is it?
+        
         tunnel match {
           case t: Water  => saving += "1\n"
           case t: Tunnel => saving += "0\n"
         }
-        tunnel.ant match { // storing specs of local ant
+        
+          // What kind of ant is living here  
+        
+        tunnel.ant match {
           case None => saving += "0\n\n\n\n"
           case Some(a) => { // type ant
             a match {
@@ -46,35 +67,66 @@ object savestate {
         saving += tunnel.bees.length + "\n" // number of bees
       }
     }
+    
+    // Now we print what we just learned
+    
     printToFile(saving)
   }
+  
+  // We now need to create the world based on what we know
+  
   def load() = {
-    val lines = Source.fromFile(fileName).getLines.toArray
+  
+    val lines = Source.fromFile(fileName).getLines.toArray // Retrieving information
+    
+    // From now on we reconstruct the world
+    
+      // General game state
+    
     AntsBees.state.purse.money = lines(0).toInt
     AntsBees.state.uniqueUnits = lines(1).toInt
     AntsBees.state.lost = lines(2).toBoolean
     AntsBees.state.timer = lines(3).toInt
+    
     // Creating 4 caves
+    
     var caves: List[Cave] = Nil
+    
     for (i <- 0 until AntsBees.state.numberCaves) {
+    
+        // Creating a specific cave
+      
       var cave = new Cave(i, AntsBees.state.hive, AntsBees.state.numberTunnels)
+      
+        // Initializing cave's tunnels
+      
       var tunnels: List[Tunnel] = Nil
-      val t0 = new Tunnel(new Point(0, 0), null, null, cave.tunnelIcon)
+      val t0 = new Tunnel(new Point(0, 0), null, null, cave.tunnelIcon) // Placeholder
       tunnels = t0 :: tunnels
+      
       // Creating 8 tunnels for the cave
+      
       for (j <- 0 until AntsBees.state.numberTunnels) {
-        var occupied = true
-        var tunnel = new Tunnel(new Point(cave.width * j, cave.altitude * cave.height + 300), tunnels.head, t0, cave.tunnelIcon)
-        var ant: Ant = new Harvester(tunnel)
+        
         val tunnelLine = 4 + (3 - i) * 48 + (7 - j) * 6 // Index of the first term of the current tunnel in saves.txt
+        
+        var tunnel = new Tunnel(new Point(cave.width * j, cave.altitude * cave.height + 300), tunnels.head, t0, cave.tunnelIcon) // Default tunnel
+        
+        var ant: Ant = new Harvester(tunnel) // Default ant
+        var occupied = true
+        
+        // Setting tunnel type
+        
         lines(tunnelLine).toInt match {
           case 0 =>
           case 1 => tunnel = new Water(new Point(cave.width * j, cave.altitude * cave.height + 300), tunnels.head, t0, cave.waterIcon)
         }
-        
         if (j == 0) { // initialization of t1
           tunnel.exit = cave.hive
         }
+        
+        // Working on ants living here
+        
         lines(tunnelLine + 1).toInt match {
           case 0  => occupied = false
           case 1  =>
@@ -95,23 +147,42 @@ object savestate {
           ant.buffed = lines(tunnelLine + 4).toBoolean
           tunnel.ant = Some(ant)
         }
+        
+        // Adding in bees
+        
         for (k <- 0 until lines(tunnelLine + 5).toInt) {
           val b = new Bee(tunnel)
           tunnel.bees = b :: tunnel.bees
         }
+        
+        // Here is the entrance of the cave
+        
         if (j == 7) {
           cave.entrance = new Entrance(new Point(AntsBees.state.numberTunnels * cave.width, cave.altitude), tunnel)
           tunnel.entrance = cave.entrance
         }
+        
+        // Adding this tunnel to the cave's tunnels
+        
         tunnels = tunnel :: tunnels
       }
-      tunnels = tunnels diff List(t0)
+      
+      tunnels = tunnels diff List(t0) // Flushing out the placeholder
+      
+      // Setting up the right entrance for every tunnel
+      
       for (i <- 1 until AntsBees.state.numberTunnels) {
         tunnels.apply(i).entrance = tunnels.apply(i - 1)
       }
+      
+      // Finalizing cave
+      
       cave.Tunnels = tunnels
       caves = cave :: caves
     }
+    
+    // Replacing cave
+    
     AntsBees.state.Caves = caves
   }
 }
